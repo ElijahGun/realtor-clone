62337c79173ddae7115c165eff3da5627cf8774a
Elijah Gundorin <elijahgundorin@Elijahs-MacBook-Pro.local>
2022-11-03 11:06:09 -0400

    listingItem component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import { MdLocationOn } from "react-icons/md";
 
-export default function ListingItem({listing, id}) {
+export default function ListingItem({ listing, id }) {
   return (
-    <div>{listing.name}</div>
-  )
+    <li className="m-[10px] relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150">
+      <Link className="contents" to={`/category/${listing.type}/${id}`}>
+        <img
+          className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
+          loading="lazy"
+          src={listing.imgUrls[0]}
+          alt=""
+        />
+        <Moment className="absolute top-2 left-2 bg-[#3377CC] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg" fromNow>{listing.timestamp?.toDate()}</Moment>
+        <div className="w-full p-[10px]">
+          <div className="flex items-center space-x-1">
+            <MdLocationOn className="h-4 w-4 text-green-600" />
+            <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">{listing.address}</p>
+          </div>
+          <p className="font-semibold mt-2 text-xl m-0 truncate">{listing.name}</p>
+          <p className=" text-[#457b9d] truncate mt-2 font-semibold">
+            $
+            {listing.offer
+              ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" && "/ month"}
+          </p>
+          <div className="flex items-center mt-[10px] space-x-3">
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}</p>
+            </div>
+            <div className="flex items-center mt-[10px] space-x-3">
+              <p className="font-bold text-xs">{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
 }
