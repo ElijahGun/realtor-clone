563f39456070ae54cfe78849ba8334089ff2fb2c
Elijah Gundorin <elijahgundorin@Elijahs-MacBook-Pro.local>
2022-11-03 11:27:52 -0400

    add delete / edit functionality on profile listings

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { MdLocationOn } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
 
-export default function ListingItem({ listing, id }) {
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className="m-[10px] relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150">
       <Link className="contents" to={`/category/${listing.type}/${id}`}>
@@ -13,11 +15,18 @@ export default function ListingItem({ listing, id }) {
           src={listing.imgUrls[0]}
           alt=""
         />
-        <Moment className="absolute top-2 left-2 bg-[#3377CC] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg" fromNow>{listing.timestamp?.toDate()}</Moment>
+        <Moment
+          className="absolute top-2 left-2 bg-[#3377CC] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
+          fromNow
+        >
+          {listing.timestamp?.toDate()}
+        </Moment>
         <div className="w-full p-[10px]">
           <div className="flex items-center space-x-1">
             <MdLocationOn className="h-4 w-4 text-green-600" />
-            <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">{listing.address}</p>
+            <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">
+              {listing.address}
+            </p>
           </div>
           <p className="font-semibold mt-2 text-xl m-0 truncate">{listing.name}</p>
           <p className=" text-[#457b9d] truncate mt-2 font-semibold">
@@ -29,14 +38,30 @@ export default function ListingItem({ listing, id }) {
           </p>
           <div className="flex items-center mt-[10px] space-x-3">
             <div className="flex items-center space-x-1">
-              <p className="font-bold text-xs">{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}</p>
+              <p className="font-bold text-xs">
+                {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+              </p>
             </div>
             <div className="flex items-center mt-[10px] space-x-3">
-              <p className="font-bold text-xs">{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
+              <p className="font-bold text-xs">
+                {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+              </p>
             </div>
           </div>
         </div>
       </Link>
+      {onDelete && (
+        <FaTrash
+          className="absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500"
+          onClick={() => onDelete(listing.id)}
+        />
+      )}
+      {onEdit && (
+        <MdEdit
+          className="absolute bottom-2 right-7 h-[14px] cursor-pointer "
+          onClick={() => onEdit(listing.id)}
+        />
+      )}
     </li>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
@@ -74,6 +74,20 @@ export default function Profile() {
     }
     fetchUserListings()
   }, [auth.currentUser.uid])
+
+  async function onDelete(listingID){
+    if(window.confirm('Are you sure you want to delete?')){
+      await deleteDoc(doc(db, 'listings', listingID))
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      )
+      setListings(updatedListings)
+      toast.success('Successfully deleted listing')
+    }
+  }
+  function onEdit(listingID){
+    navigate(`/edit-listing/${listingID}`)
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -138,7 +152,10 @@ export default function Profile() {
             <h2 className="text-2xl text-center font-semibold mb-6 ">My Listings</h2>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6">
               {listings.map((listing)=> (
-                <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
+                <ListingItem key={listing.id} id={listing.id} listing={listing.data}
+                onDelete={()=>onDelete(listing.id)}
+                onEdit={()=>onEdit(listing.id)}
+                 />
               ))}
             </ul>
           </>
